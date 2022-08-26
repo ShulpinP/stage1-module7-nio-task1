6bde0073d3a52a1437781ee838300ca264c7f5ee
ShulpinP <pavelshulpin6@gmail.com>
2022-08-26 23:26:17 +0300

    1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,7 +1,6 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -21,7 +20,7 @@ public class FileReader {
         StringBuilder data = new StringBuilder();
         Map<String,String> map = new HashMap<>();
 
-        try (RandomAccessFile aFile = new RandomAccessFile(file, "r");) {
+        try (RandomAccessFile aFile = new RandomAccessFile(file, "r")) {
         FileChannel inChannel = aFile.getChannel();
         ByteBuffer buffer = ByteBuffer.allocate(1024);
         char ch;
